83ff0eff9239a7bb123580ff9c2b9b6f2227b2df
Ludoviko <louis@aplicared.co>
2020-08-06 21:12:26 +0100

    Initial Commit

---
+++ Shared/MCGvidiloApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCGvidiloApp.swift
+//  Shared
+//
+//  Created by Louis Hollingworth on 6/8/20.
+//
+
+import SwiftUI
+
+@main
+struct MCGvidiloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
